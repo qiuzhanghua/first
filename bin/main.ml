2a26161e184d67484fb534b8e36d1df45d177a49
qiuzhanghua <qiuzhanghua@icloud.com>
2024-01-06 19:38:02 +0800

    feat: owl network

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,8 @@
 open First
+open Owl
+open Neural.S
+open Neural.S.Graph
+open Neural.S.Algodiff
 
 let () =
   let x = Learn.add 1 2 in
@@ -7,4 +11,18 @@ let () =
   Printf.printf "Power of 10 = %s\n"
     ("[" ^ String.concat "; " (List.map string_of_int ps) ^ "]")
 
-let () = ()
+let make_network input_shape =
+  input input_shape
+  |> lambda (fun x -> Maths.(x / F 256.))
+  |> conv2d [| 5; 5; 1; 32 |] [| 1; 1 |] ~act_typ:Activation.Relu
+  |> max_pool2d [| 2; 2 |] [| 2; 2 |]
+  |> dropout 0.1
+  |> fully_connected 1024 ~act_typ:Activation.Relu
+  |> linear 10 ~act_typ:Activation.(Softmax 1)
+  |> get_network
+
+let network = make_network [| 28; 28; 1 |]
+
+let _ =
+  Graph.print network;
+  print_newline ()
